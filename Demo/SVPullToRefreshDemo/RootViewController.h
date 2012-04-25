4949886729b2df3466bce8f2d23f1ed9fd6d4dbb
Christopher Pickslay <cpickslay@twobitlabs.com>
2012-04-25 11:09:48 -0700

    added navigation controller to allow for testing retain cycle

---
+++ Demo/SVPullToRefreshDemo/RootViewController.h
@@ -0,0 +1,14 @@
+//
+//  RootViewController.h
+//  SVPullToRefreshDemo
+//
+//  Created by Christopher Pickslay on 4/24/12.
+//  Copyright (c) 2012 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+- (IBAction)didTapLaunch:(id)sender;
+
+@end
