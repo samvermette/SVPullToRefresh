1905fd433ec38dfbfb170872cdb90b3c23f1741e
Sam Vermette <samvermette@gmail.com>
2012-09-04 03:20:44 -0400

    Fix dealloc not getting called when scrollView is removed from view stack.

--- SVPullToRefresh/SVPullToRefresh.m
+++ SVPullToRefresh/SVPullToRefresh.m
@@ -402,6 +402,16 @@ static char UIScrollViewInfiniteScrollingView;
 
 @dynamic pullToRefreshView, showsPullToRefresh, infiniteScrollingView, showsInfiniteScrolling;
 
+- (void)willMoveToSuperview:(UIView *)newSuperview {
+    if(!newSuperview) {
+        [self.pullToRefreshView removeFromSuperview];
+        self.pullToRefreshView = nil;
+        
+        [self.infiniteScrollingView removeFromSuperview];
+        self.infiniteScrollingView = nil;
+    }
+}
+
 - (void)addPullToRefreshWithActionHandler:(void (^)(void))actionHandler {
     self.pullToRefreshView.pullToRefreshActionHandler = actionHandler;
 }
