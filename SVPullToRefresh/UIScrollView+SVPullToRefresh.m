36b25d9ca28b312dd11b7d51d137a906bce624d0
Gabriele Petronella <gabriele.petronella@gmail.com>
2013-05-25 14:48:34 -0500

    When resetting the original insets, only the relevant one is not reset. Resetting both was interfering with infinite scrolling

--- SVPullToRefresh/UIScrollView+SVPullToRefresh.m
+++ SVPullToRefresh/UIScrollView+SVPullToRefresh.m
@@ -327,7 +327,6 @@ static char UIScrollViewPullToRefreshView;
             break;
         case SVPullToRefreshPositionBottom:
             currentInsets.bottom = self.originalBottomInset;
-            currentInsets.top = self.originalTopInset;
             break;
     }
     [self setScrollViewContentInset:currentInsets];
