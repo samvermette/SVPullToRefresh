27ec582ed3e0b4b6ca1b51c5f9314d207fccebbf
mrdaios <mrdaios@gmail.com>
2014-04-25 16:38:16 +0800

    Sometimes SVPullToRefreshView and SVInfiniteScrollingView exist. So fix the coordinates of SVInfiniteScrollingView。

--- SVPullToRefresh/UIScrollView+SVInfiniteScrolling.m
+++ SVPullToRefresh/UIScrollView+SVInfiniteScrolling.m
@@ -187,13 +187,13 @@ UIEdgeInsets scrollViewOriginalContentInsets;
         [self scrollViewDidScroll:[[change valueForKey:NSKeyValueChangeNewKey] CGPointValue]];
     else if([keyPath isEqualToString:@"contentSize"]) {
         [self layoutSubviews];
-        self.frame = CGRectMake(0, self.scrollView.contentSize.height, self.bounds.size.width, SVInfiniteScrollingViewHeight);
+        self.frame = CGRectMake(0, MAX(self.scrollView.contentSize.height, self.scrollView.bounds.size.height), self.bounds.size.width, SVInfiniteScrollingViewHeight);
     }
 }
 
 - (void)scrollViewDidScroll:(CGPoint)contentOffset {
     if(self.state != SVInfiniteScrollingStateLoading && self.enabled) {
-        CGFloat scrollViewContentHeight = self.scrollView.contentSize.height;
+        CGFloat scrollViewContentHeight = MAX(self.scrollView.contentSize.height, self.scrollView.bounds.size.height);
         CGFloat scrollOffsetThreshold = scrollViewContentHeight-self.scrollView.bounds.size.height;
         
         if(!self.scrollView.isDragging && self.state == SVInfiniteScrollingStateTriggered)
