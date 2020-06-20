c69e6898d6faad37abea0d4240a19ba82a0b16ec
janlionly <janlionly@gmail.com>
2020-06-20 18:25:42 +0800

    feat: support for SwiftPM

---
+++ Tests/SVPullToRefreshTests/SVPullToRefreshTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import SVPullToRefresh
+
+final class SVPullToRefreshTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SVPullToRefresh().text, "Hello, World!")
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
